ffd50ffba878badbed075cd527f0e00d2ecc5e45
Madang Garden <chooi9522@gmail.com>
2020-12-30 17:07:11 +0900

    #1.1 Searching

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get("https://google.com")
+
+
+search_bar = browser.find_element_by_class_name("gLFyf")
+
+print(search_bar)
+
+search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
+
+search_results = browser.find_elements_by_class_name("g")
+
+print(search_results)
