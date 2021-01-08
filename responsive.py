96017b37dbf7a945460b7a40fd9112c7f278c632
Madang Garden <chooi9522@gmail.com>
2021-01-09 02:24:22 +0900

    #2.2 Recap and Screenshots

--- responsive.py
+++ responsive.py
@@ -19,9 +19,11 @@ sizes = [
 
 for size in sizes :
     browser.set_window_size(size, BROWSER_HEIGHT)
+    browser.execute_script("window.scrollTo(0,0)")
     time.sleep(3)
     scroll_size = browser.execute_script("return document.body.scrollHeight")
     total_sections = ceil(scroll_size / BROWSER_HEIGHT)
-    for section in range(total_sections):
+    for section in range(total_sections + 1):
         browser.execute_script(f"window.scrollTo(0,{(section+1) * BROWSER_HEIGHT})")
+        browser.save_screenshot(f"screenshots/{size}x{section+1}.png")
         time.sleep(2)
