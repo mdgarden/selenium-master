ec6a47ae6f27f1c022932ae7a43a70045e1770e4
Madang Garden <chooi9522@gmail.com>
2021-01-10 03:59:48 +0900

    Done / #3.7 Extras

--- responsive.py
+++ responsive.py
@@ -1,33 +1,48 @@
-import time
-from math import ceil
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-class ResponsiveTester:
-    def __init__(self, urls):
-        self.browser = webdriver.Chrome(ChromeDriverManager().install())
-        self.browser.maximize_window()
-        self.urls = urls
-        self.sizes = [480, 960, 1366, 1920]
+class GoogleKeywordScreenshooter:
+    def __init__(self, keyword, screenshots_dir):
+        options = Options()
+        options.add_argument("--headless")
+        self.browser = webdriver.Chrome(
+            ChromeDriverManager().install(), options=options
+        )
+        self.keyword = keyword
+        self.screenshots_dir = screenshots_dir
 
-
-    def screenshot(self, url):
-            BROWSER_HEIGHT = 1027
-            self.browser.get(url)
-            for size in self.sizes:
-                self.browser.set_window_size(size, BROWSER_HEIGHT)
-                self.browser.execute_script("window.scrollTo(0, 0)")
-                time.sleep(3)
-                scroll_size = self.browser.execute_script(
-                    "return document.body.scrollHeight"
-                )
-                total_sections = ceil(scroll_size / BROWSER_HEIGHT)
-                for section in range(total_sections + 1):
-                    self.browser.execute_script(
-                        f"window.scrollTo(0, {section * BROWSER_HEIGHT})"
-                    )
-                    time.sleep(2)
-                    self.browser.save_screenshot(f"screenshots/{size}x{section}.png")
-
-tester = ResponsiveTester(["https://nomadcoders.co", "https://google.com"])
-tester.start()
+    def start(self):
+        self.browser.get("https://google.com")
+        search_bar = self.browser.find_element_by_class_name("gLFyf")
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
+        try:
+            shitty_element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+            )
+            self.browser.execute_script(
+                """
+            const shitty = arguments[0];
+            shitty.parentElement.removeChild(shitty)
+            """,
+                shitty_element,
+            )
+        except Exception:
+            pass
+        search_results = self.browser.find_elements_by_class_name("g")
+        for index, search_result in enumerate(search_results):
+            search_result.screenshot(
+                f"{self.screenshots_dir}/{self.keyword}x{index}.png"
+            )
+        self.finish()
+    def finish(self):
+        self.browser.quit()
+domain_competitors = GoogleKeywordScreenshooter("buy domain", "screenshots")
+domain_competitors.start()
+python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
+python_competitors.start()
